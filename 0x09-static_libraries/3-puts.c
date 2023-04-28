1b775e99d341696e772cddbcd7de6a08bc38f0b0
inflectino <123831532+inflectino@users.noreply.github.com>
2023-04-28 20:01:22 +0000

    static-libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- *_puts - prints a string
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
- *
- * Description: prints a string
- * On success: return the number of characters printed
  */
-
 void _puts(char *str)
 {
-while (*str)
-_putchar(*str++);
-
-_putchar('\n');
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
 }
